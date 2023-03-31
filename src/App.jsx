9048d93327330795c643bbac8e7cbc845ec314c1
Washim-Akram <muhammadwashimakram@gmail.com>
2023-03-31 14:58:41 +0600

    Create blogs component and fetch data from json file using useEffect hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Blogs from './components/Blogs/Blogs';
 import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Blogs></Blogs>
     </div>
   )
 }
